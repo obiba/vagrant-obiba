1225f717ab7a2c951915e426086e183130443acd
Yannick Marcon <yannick.marcon@obiba.org>
2013-06-26 17:37:37 -0400

    vagrant opal added.

---
+++ opal/install-opal.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+VAGRANT_DATA=/vagrant/data
+
+source $VAGRANT_DATA/settings
+
+wget -q -O - http://pkg.obiba.org/obiba.org.key | apt-key add -
+echo "deb http://pkg.obiba.org stable/" >> /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+echo "deb http://cran.rstudio.com/bin/linux/ubuntu precise/" >> /etc/apt/sources.list
+sudo apt-get update
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password rootpass'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password rootpass'
+sudo apt-get -y install mysql-server 
+
+sudo debconf-set-selections <<< 'opal opal-server/admin_password password pass246'
+sudo debconf-set-selections <<< 'opal opal-server/admin_password_again password pass246'
+sudo apt-get -y install opal 
+#sudo apt-get -y install opal-python-client
+
+sudo apt-get -y install r-base
+sudo apt-get -y install r-cran-rserve
+
+
+# Opal database setup
+if [ -f $VAGRANT_DATA/mysql/my.cnf ];
+then
+	sudo cp $VAGRANT_DATA/mysql/my.cnf /etc/mysql
+	sudo service mysql restart
+fi
+
+echo "CREATE DATABASE opal_data CHARACTER SET utf8" | mysql -uroot -prootpass
+echo "CREATE DATABASE opal_ids CHARACTER SET utf8" | mysql -uroot -prootpass
+echo "CREATE USER 'opaluser'@'localhost' IDENTIFIED BY 'opalpass'" | mysql -uroot -prootpass
+echo "GRANT ALL ON opal_data.* TO 'opaluser'@'localhost'" | mysql -uroot -prootpass
+echo "GRANT ALL ON opal_ids.* TO 'opaluser'@'localhost'" | mysql -uroot -prootpass
+echo "FLUSH PRIVILEGES" | mysql -uroot -prootpass
+
+if [ -f $VAGRANT_DATA/mysql/opal_data-initial.sql ];
+then
+	mysql -uroot -prootpass opal_data < $VAGRANT_DATA/mysql/opal_data-initial.sql
+fi
+
+if [ -f $VAGRANT_DATA/mysql/opal_ids-initial.sql ];
+then
+	mysql -uroot -prootpass opal_ids < $VAGRANT_DATA/mysql/opal_ids-initial.sql
+fi
+
+
+# Opal configuration setup
+if [ -d $VAGRANT_DATA/opal/conf ];
+then
+	sudo cp -r $VAGRANT_DATA/opal/conf/* /etc/opal
+	sudo chown -R opal:adm /etc/opal
+	sudo service opal restart
+fi
+
+if [ -d $VAGRANT_DATA/opal/fs ];
+then
+	sudo cp -r $VAGRANT_DATA/opal/fs/* /var/lib/opal/fs
+	sudo chown -R opal:nogroup /var/lib/opal/fs
+fi
+
+
+# R server setup
+if [ -f $VAGRANT_DATA/r/rserve ];
+then
+	sudo adduser --system --home /var/lib/rserve --disabled-password rserve
+	sudo cp $VAGRANT_DATA/r/rserve /etc/init.d
+	sudo chmod +x /etc/init.d/rserve
+	sudo update-rc.d rserve defaults
+	sudo service rserve start
+fi
+
+# R studio setup
+wget http://download2.rstudio.org/$RSTUDIO
+sudo apt-get -y install libssl0.9.8
+sudo dpkg -i $RSTUDIO
+
+sudo cp /usr/lib/rstudio-server/extras/init.d/debian/rstudio-server /etc/init.d
+sudo update-rc.d rstudio-server defaults
