91104d2cfddc5306411f8485c9c6f3cf3096e7f0
Yannick Marcon <yannick.marcon@obiba.org>
2014-03-13 13:23:50 -0400

    Fix some opal stable scripts

--- scripts/opal/add-obiba-stable-apt-repo.sh
+++ scripts/opal/add-obiba-stable-apt-repo.sh
@@ -2,6 +2,6 @@
 
 if [ $(grep -c '^deb http://pkg.obiba.org stable/' /etc/apt/sources.list) -eq 0 ];
 then
-	wget -q -O - http://pkg.obiba.org/obiba.org.key | apt-key add -
+	wget -q -O - http://pkg.obiba.org/obiba.org.key | sudo apt-key add -
 	sudo sh -c 'echo "deb http://pkg.obiba.org stable/" >> /etc/apt/sources.list'
 fi
